fb584c911af18c1f9b7acf9c9dd76b9a972512fb
Andres Rpo <andresr.alz@gmail.com>
2016-11-22 14:53:02 -0500

    error handler, new method to return info abour table x

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var util = require('util');
 var security = require('../security/basic_auth');
 var dbconnection = require('../dbconnection');
 var router = express.Router();
@@ -10,14 +11,43 @@ router.get('/', security.auth, function (req, res, next) {
 
 router.get('/list-tables', security.auth, function (req, res, next) {
     var connection = dbconnection.getConnection();
-    connection.query('show tables like \'%LUP%\';', function (err, rows, fields) {
+    var query = 'show tables like \'%LUP%\'';
+    connection.query(query, function (err, rows, fields) {
         if (err) throw err;
         var tableNames = [];
         for (var i in rows) {
-            val = rows[i]['Tables_in_brainspa (%LUP%)'];
+            var val = rows[i]['Tables_in_brainspa (%LUP%)'];
             tableNames.push(val);
         }
         res.json(tableNames);
+    });
+    connection.end();
+});
+
+
+router.get('/list-tables/:tableName', security.auth, function (req, res, next) {
+    var connection = dbconnection.getConnection();
+    var tableName = req.params["tableName"];
+    var query = util.format("DESCRIBE %s", tableName);
+    connection.query(query, function (err, rows, fields) {
+        if (err) return next(err);
+        var result = {
+            "table_name": tableName,
+            "fields": []
+        };
+        for (var i in rows) {
+            var field = rows[i]['Field'];
+            var type = rows[i]['Type'];
+            var nullable = rows[i]['Null'];
+            var key = rows[i]['Key'];
+            result.fields.push({
+                "field": field,
+                "type": type,
+                "nullable": nullable,
+                "key": key
+            });
+        }
+        res.json(result);
     });
     connection.end();
 });
