7a102b97bc0ab6483dda10f5742484e271d1fe50
Andres Rpo <andresr.alz@gmail.com>
2016-11-23 14:51:20 -0500

    add new resources, update, save, modified resource get-data to return all fields always, in test err no paint template return json

--- api/dbconnection.js
+++ api/dbconnection.js
@@ -1,13 +1,25 @@
 var mysql = require('mysql');
+var util = require('util');
+
 
 var getConnection = function () {
     var connection = mysql.createConnection({
         host: '104.196.180.164',
         user: 'root',
         password: 'racerx09!',
-        database: 'brainspa'
+        database: 'brainspa',
+        multipleStatements: true
     });
     return connection;
 };
 
+var getPrimaryKeyFromTable = function(connection, tableName, done){
+    var query = util.format('show index from %s where Key_name = \'PRIMARY\'', tableName);
+    connection.query(query, function (err, rows, fields) {
+        if (err) throw Error("error getting index info");
+        done(rows[0]);
+    });
+};
+
 module.exports.getConnection = getConnection;
+module.exports.getPrimaryKeyFromTable = getPrimaryKeyFromTable;
--- api/routes/index.js
+++ api/routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var util = require('util');
 var security = require('../security/basic_auth');
 var dbconnection = require('../dbconnection');
+var mysql = require('mysql');
 var router = express.Router();
 
 
@@ -57,23 +58,66 @@ router.get('/get-data/:tableName', security.auth, function (req, res, next) {
     var connection = dbconnection.getConnection();
     var tableName = req.params["tableName"];
 
-    var fields_params = (req.query.fields) ? req.query.fields.split(",") : next(new Error('fields is required :-('));
     var query = util.format("select * from %s", tableName);
     connection.query(query, function (err, rows, fields) {
         if (err) return next(err);
         var result = [];
         for (var i in rows) {
-            var obj = {};
-            for (k in fields_params){
-                var paramName  = fields_params[k];
-                var paramValue = rows[i][fields_params[k]];
-                obj[paramName] = paramValue
-            }
-            result.push(obj);
+            result.push(rows[0]);
         }
         res.json(result);
     });
     connection.end();
+});
+
+router.post('/save/:tableName', security.auth, function (req, res, next) {
+    var body = req.body;
+    var tableName = req.params["tableName"];
+
+    var obj = {};
+    for (i in body.fields) {
+        var field = body.fields[i];
+        obj[field.fieldName] = field.fieldValue;
+    }
+
+    var insertQuery = util.format("INSERT INTO %s SET ?", tableName);
+    var connection = dbconnection.getConnection();
+
+    connection.query(insertQuery, obj, function (err, result) {
+        if (err) return next(err);
+        res.json({"affectedRows": result["affectedRows"]});
+    });
+    connection.end();
+});
+
+router.post('/update/:tableName', security.auth, function (req, res, next) {
+    var body = req.body;
+    var tableName = req.params["tableName"];
+
+    var connection = dbconnection.getConnection();
+    dbconnection.getPrimaryKeyFromTable(connection, tableName, function (data) {
+        var indexName = data["Column_name"];
+        var values = [];
+        for (i in body.fields) {
+            var field = body.fields[i];
+            var obj = {};
+            if (field.fieldName === indexName) continue;
+            obj[field.fieldName] = field.fieldValue;
+            values.push(obj);
+        }
+
+        var queries = '';
+        values.forEach(function (item) {
+            var queryUpdate = util.format("UPDATE %s SET ? where %s = '%s';",
+                tableName, indexName, body.primaryKeyValue);
+            queries += mysql.format(queryUpdate, item);
+        });
+        connection.query(queries, function (err, result) {
+            if (err) return next(err);
+            res.json({"affectedRows": result["affectedRows"]});
+        });
+        connection.end();
+    });
 });
 
 
