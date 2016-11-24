40927efd3fa862b2d008ed92a4774bec43a3ab37
alexander <alexander.agudelo@linkapedia.com>
2016-11-24 17:32:36 -0500

    changes icons and user avatar

--- site/public/app/js/brainspa-layout.js
+++ site/public/app/js/brainspa-layout.js
@@ -1,6 +1,6 @@
 var BRAINSPA_LAYOUT = {
     menuItemTable: function (tableName) {
-        var template = '<li table-name="{0}" title="{0}"><a href="#"><i class="zmdi zmdi-folder-outline"></i> <p>{0}</p></a></li>';
+        var template = '<li table-name="{0}" title="{0}"><a href="#"><i class="zmdi"></i> <p>{0}</p></a></li>';
 
         return $(template.format(tableName));
     },
